c28817fd4202db9da240ae926ad99175f7da0c73
Hugo <hugopessanha22@gmail.com>
2022-07-01 17:51:30 -0300

    CHAMADO COMPONENT FINALIZANDO

--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -1,6 +1,8 @@
 import { API_CONFIG } from './../config/api.config';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Chamado } from '../Models/Chamado';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,22 @@ import { Injectable } from '@angular/core';
 export class ChamadoService {
 
   baseUrl = API_CONFIG.baseUrl + '/chamados';
-  
+
   constructor(private http: HttpClient) { }
+
+  findById(id: any): Observable<Chamado> {
+    return this.http.get<Chamado>(`${this.baseUrl}/${id}`);
+  }
+
+  findAll(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(`${this.baseUrl}`);
+  }
+
+  create(chamado: Chamado): Observable<Chamado> {
+    return this.http.post<Chamado>(`${this.baseUrl}`, chamado);
+  }
+
+  update(chamado: Chamado): Observable<Chamado> {
+    return this.http.put<Chamado>(`${this.baseUrl}/${chamado.id}`, chamado);
+  }
 }
